92fc42bc06cb3b26d7eb63e62cfa5cc494c41a6f
gia <giadatdoan@gmail.com>
2022-01-25 18:55:23 -0500

    added custom logo

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,30 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <header className="">
-      <h1>Header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <Image
         className="object-contain"
-        src="https://links.papareact.com/ua6"
+        src="/images/notflix.png"
         width={200}
         height={100}
         alt="logo"
       />
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
     </header>
   );
 };
